12b27cdd3b641692d24973df1cc125b133991a62
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2023-02-12 12:02:40 +0100

    add button component

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Btn_base from './components/btn_base.vue';
+
+const onlineStatus = "away";
+
+
+</script>
+
+<template>
+  <div>
+    <Btn_base class="btn" :class="onlineStatus" buttonText="offline"></Btn_base>
+    <Btn_base class="btn" buttonText="Join us"></Btn_base>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  display: flex;
+}
+</style>
---
+++ src/components/btn_base.vue
@@ -0,0 +1,99 @@
+<script setup>
+
+defineProps({
+    buttonText: String
+})
+</script>
+
+<template>
+
+    <button>{{ buttonText }}</button>
+
+</template>
+
+<style lang="scss" scoped>
+div {
+    display: flex;
+
+}
+
+
+
+.btn {
+    width: 100px;
+    padding: 5px 10px;
+    margin: 10px;
+    border-radius: 5px;
+    border-width: 2px;
+    background-color: rgb(24, 202, 24);
+    border-color: transparent;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    transition: .5s;
+    cursor: pointer;
+
+    color: white;
+    font-weight: 600;
+    font-family: 'Segoe UI',
+        Tahoma,
+        Geneva,
+        Verdana,
+        sans-serif;
+
+
+    &:hover {
+        box-shadow: 0px 0px 10px rgba(197, 193, 193, 0.726);
+    }
+
+
+
+    &.online {
+        border-color: white;
+        background-color: transparent;
+
+        &::before {
+            content: "";
+            display: flex;
+            width: 7px;
+            height: 7px;
+            border-radius: 50%;
+            margin-right: 10px;
+            background-color: rgb(24, 202, 24);
+        }
+    }
+
+    &.away {
+        border-color: white;
+        background-color: transparent;
+
+        &::before {
+            content: "";
+            display: flex;
+            width: 7px;
+            height: 7px;
+            border-radius: 50%;
+            margin-right: 10px;
+            background-color: rgb(255, 217, 0);
+        }
+    }
+
+    &.offline {
+        border-color: white;
+        background-color: transparent;
+
+        &::before {
+            content: "";
+            display: flex;
+            width: 7px;
+            height: 7px;
+            border-radius: 50%;
+            margin-right: 10px;
+            background-color: rgb(202, 24, 24);
+        }
+    }
+
+
+}
+</style>
